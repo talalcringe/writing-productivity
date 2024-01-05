ecea675141d361864cf1c534515c8985025b910a
talalcringe <104639094+talalcringe@users.noreply.github.com>
2024-01-06 01:12:59 +0500

    back

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -71,7 +71,9 @@ exports.createPageFolder = async (req, res, next) => {
 //create Text Files And Upload to drive
 exports.createTextFilesAndUpload = async (req, res, next) => {
   const { token } = req.userData;
+  console.log('here1');
   try {
+    console.log('here1');
     const { updatedDataFormat } = req.body;
     const { max, pagenum, text } = updatedDataFormat;
     console.log('updatedDataFormat', updatedDataFormat);
